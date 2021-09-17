89ebe3b6db5254c812648b9c54b3083ae43622a8
Nicolas Codonho Scorpelini <50plantasquemudaramahistoria@gmail.com>
2021-09-17 18:34:11 -0300

    Refactored ifs statements and created README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Chatapp WebSocket
+
+A simplest chatapp made with PHP and html/JS/CSS
+
+Thanks to [Zhenbianshu](https://github.com/zhenbianshu/websocket) without your repo I can't make this app.
--- src/Websocket.php
+++ src/Websocket.php
@@ -2,27 +2,24 @@
 
 namespace ChatApp\Websocket;
 
+error_reporting(E_ALL);
+set_time_limit(0);
+
 class WebSocket {
     const LOG_PATH = '/tmp/';
     const LISTEN_SOCKET_NUM = 9;
-
-    
     private $sockets = [];
     private $main;
 
     public function __construct($host, $port) {
         try {
             $this->main = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-            
             socket_set_option($this->main, SOL_SOCKET, SO_REUSEADDR, 1);
-            
             socket_bind($this->main, $host, $port);
-            
             socket_listen($this->main, self::LISTEN_SOCKET_NUM);
         } catch (\Exception $e) {
             $err_code = socket_last_error();
             $err_msg = socket_strerror($err_code);
-
             $this->error([
                 'error_init_server',
                 $err_code,
@@ -62,42 +59,40 @@ class WebSocket {
         }
 
         foreach ($sockets as $socket) {
-            
+
             if ($socket == $this->main) {
                 $client = socket_accept($this->main);
-                
-                if (false === $client) {
+                $bytes = @socket_recv($socket, $buffer, 2048, 0);
+                               
+                if (!$client) {
                     $this->error([
                         'err_accept',
                         $err_code = socket_last_error(),
                         socket_strerror($err_code)
                     ]);
                     continue;
-                } else {
-                    $this->connect($client);
-                    continue;
                 }
-            } else {
-                
-                $bytes = @socket_recv($socket, $buffer, 2048, 0);
-                if ($bytes < 9) {
-                    $recv_msg = $this->disconnect($socket);
-                } else {
-                    if (!$this->sockets[(int)$socket]['handshake']) {
-                        $this->handShake($socket, $buffer);
-                        continue;
-                    } else {
-                        $recv_msg = $this->parse($buffer);
-                    }
-                }
-                array_unshift($recv_msg, 'receive_msg');
-                $msg = $this->dealMsg($socket, $recv_msg);
 
-                $this->broadcast($msg);
+                $this->connect($client);
+                continue;
+            } 
+                
+            $bytes = @socket_recv($socket, $buffer, 2048, 0);
+            if ($bytes < 9) {
+                $recv_msg = $this->disconnect($socket);
+            } 
+
+            if (!$this->sockets[(int)$socket]['handshake']) {
+                $this->handShake($socket, $buffer);
+                continue;
             }
+            
+            $recv_msg = $this->parse($buffer);
+            array_unshift($recv_msg, 'receive_msg');
+            $msg = $this->dealMsg($socket, $recv_msg);
+            $this->broadcast($msg);
         }
     }
-
     
     public function connect($socket) {
         socket_getpeername($socket, $ip, $port);
@@ -117,16 +112,14 @@ class WebSocket {
             'type' => 'logout',
             'content' => $this->sockets[(int)$socket]['uname'],
         ];
-        unset($this->sockets[(int)$socket]);
 
+        unset($this->sockets[(int)$socket]);
         return $recv_msg;
     }
 
     public function handShake($socket, $buffer) {
-        
         $line_with_key = substr($buffer, strpos($buffer, 'Sec-WebSocket-Key:') + 18);
         $key = trim(substr($line_with_key, 0, strpos($line_with_key, "\r\n")));
-
         
         $upgrade_key = base64_encode(sha1($key . "258EAFA5-E914-47DA-95CA-C5AB0DC85B11", true));
         $upgrade_message = "HTTP/1.1 101 Switching Protocols\r\n";
@@ -146,11 +139,11 @@ class WebSocket {
             $port
         ]);
 
-        
         $msg = [
             'type' => 'handshake',
             'content' => 'done',
         ];
+
         $msg = $this->build(json_encode($msg));
         socket_write($socket, $msg, strlen($msg));
         return true;
@@ -159,16 +152,23 @@ class WebSocket {
     private function parse($buffer) {
         $decoded = '';
         $len = ord($buffer[1]) & 127;
-        if ($len === 126) {
-            $masks = substr($buffer, 4, 4);
-            $data = substr($buffer, 8);
-        } else if ($len === 127) {
-            $masks = substr($buffer, 10, 4);
-            $data = substr($buffer, 14);
-        } else {
-            $masks = substr($buffer, 2, 4);
-            $data = substr($buffer, 6);
+
+        switch ($len) {
+            case $len === 126:
+                $masks = substr($buffer, 4, 4);
+                $data = substr($buffer, 8);
+                break;
+            
+            case $len === 127:
+                $masks = substr($buffer, 10, 4);
+                $data = substr($buffer, 14);
+
+            default:
+                $masks = substr($buffer, 2, 4);
+                $data = substr($buffer, 6);
+                break;
         }
+
         for ($index = 0; $index < strlen($data); $index++) {
             $decoded .= $data[$index] ^ $masks[$index % 4];
         }
@@ -180,21 +180,30 @@ class WebSocket {
         $frame = [];
         $frame[0] = '81';
         $len = strlen($msg);
-        if ($len < 126) {
-            $frame[1] = $len < 16 ? '0' . dechex($len) : dechex($len);
-        } else if ($len < 65025) {
-            $s = dechex($len);
-            $frame[1] = '7e' . str_repeat('0', 4 - strlen($s)) . $s;
-        } else {
-            $s = dechex($len);
-            $frame[1] = '7f' . str_repeat('0', 16 - strlen($s)) . $s;
+        
+        switch ($len){
+            case $len < 126:
+                $frame[1] = $len < 16 ? '0' . dechex($len) : dechex($len);
+                break;
+                
+            case $len < 65025:
+                $s = dechex($len);
+                $frame[1] = '7e' . str_repeat('0', 4 - strlen($s)) . $s;
+                break;
+
+            default:
+                $s = dechex($len);
+                $frame[1] = '7f' . str_repeat('0', 16 - strlen($s)) . $s;    
+                break;
         }
 
         $data = '';
         $l = strlen($msg);
+
         for ($i = 0; $i < $l; $i++) {
             $data .= dechex(ord($msg[$i]));
         }
+
         $frame[2] = $data;
 
         $data = implode('', $frame);
@@ -202,36 +211,35 @@ class WebSocket {
         return pack("H*", $data);
     }
 
+    private function msgType($type, $socket, $msgContent){
+        $response = [];
+
+        if($type == 'login')
+        {
+            $this->sockets[(int)$socket]['uname'] = $msgContent;
+        }
+        
+        if($type == 'user')
+        {
+            $uname = $this->sockets[(int)$socket]['uname'];
+            $response['type'] = 'user';
+            $response['from'] = $uname;
+            $response['content'] = $msgContent;
+        }
+
+        $user_list = array_column($this->sockets, 'uname');
+        $response['type'] = $type;
+        $response['content'] = $msgContent;
+        $response['user_list'] = $user_list;
+
+        return $response;
+    }
 
     private function dealMsg($socket, $recv_msg) {
         $msg_type = $recv_msg['type'];
-        $msg_content = $recv_msg['content'];
-        $response = [];
+        $msgContent = $recv_msg['content'];
 
-        switch ($msg_type) {
-            case 'login':
-                $this->sockets[(int)$socket]['uname'] = $msg_content;
-                
-                $user_list = array_column($this->sockets, 'uname');
-                $response['type'] = 'login';
-                $response['content'] = $msg_content;
-                $response['user_list'] = $user_list;
-                break;
-            case 'logout':
-                $user_list = array_column($this->sockets, 'uname');
-                $response['type'] = 'logout';
-                $response['content'] = $msg_content;
-                $response['user_list'] = $user_list;
-                break;
-            case 'user':
-                $uname = $this->sockets[(int)$socket]['uname'];
-                $response['type'] = 'user';
-                $response['from'] = $uname;
-                $response['content'] = $msg_content;
-                break;
-        }
-
-        return $this->build(json_encode($response));
+        return $this->build(json_encode($this->msgType($msg_type, $socket, $msgContent)));
     }
 
     private function broadcast($data) {
@@ -246,7 +254,6 @@ class WebSocket {
     private function debug(array $info) {
         $time = date('Y-m-d H:i:s');
         array_unshift($info, $time);
-
         $info = array_map('json_encode', $info);
         file_put_contents(self::LOG_PATH . 'websocket_debug.log', implode(' | ', $info) . "\r\n", FILE_APPEND);
     }
@@ -254,7 +261,6 @@ class WebSocket {
     private function error(array $info) {
         $time = date('Y-m-d H:i:s');
         array_unshift($info, $time);
-
         $info = array_map('json_encode', $info);
         file_put_contents(self::LOG_PATH . 'websocket_error.log', implode(' | ', $info) . "\r\n", FILE_APPEND);
     }
